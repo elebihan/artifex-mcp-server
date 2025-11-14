ef43d045c36e28b29651be9cb51f8bdbe7d8ab68
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2025-11-14 23:44:23 +0100

    xtask: move "prepare" to dedicated module

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,51 +4,11 @@
 // SPDX-License-Identifier: MIT
 //
 
-#[cfg(unix)]
-use std::os::unix::prelude::PermissionsExt;
-use std::{env, error::Error, fs};
+mod prepare;
 
+use prepare::prepare;
+use std::{env, error::Error};
 use xshell::Shell;
-
-fn install_hooks(shell: &Shell) -> Result<(), Box<dyn Error>> {
-    let cwd = shell.current_dir();
-    let src = cwd.join("hooks");
-    let hooks = fs::read_dir(src)?
-        .filter_map(|e| e.ok())
-        .filter_map(|e| match e.file_type() {
-            Ok(t) if t.is_file() => Some(e),
-            _ => None,
-        })
-        .filter_map(|e| {
-            let path = e.path();
-            match path.extension() {
-                Some(e) if e == "hook" => Some(path),
-                _ => None,
-            }
-        });
-    for hook in hooks {
-        let name = hook
-            .file_stem()
-            .ok_or_else(|| "Missing file stem".to_string())?;
-        let mut dst = cwd.join(".git");
-        dst.push("hooks");
-        dst.push(name);
-        println!("📂 Installing Git hook: {}", &hook.display());
-        shell.copy_file(hook, &dst)?;
-        #[cfg(unix)]
-        {
-            let mut permissions = fs::metadata(&dst)?.permissions();
-            permissions.set_mode(0o755);
-            fs::set_permissions(dst, permissions)?;
-        }
-    }
-    Ok(())
-}
-
-fn prepare(shell: &Shell) -> Result<(), Box<dyn Error>> {
-    println!("🥁 Preparing build environment...");
-    install_hooks(shell)
-}
 
 fn usage() {
     eprintln!(
